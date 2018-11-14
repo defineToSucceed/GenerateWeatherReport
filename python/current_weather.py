ba2b81d3ac419b7c453cb686168bd43c3eeb43fc
Ravi <rhonakere@salesforce.com>
2018-11-14 14:07:33 -0800

    optmized

--- python/current_weather.py
+++ python/current_weather.py
@@ -2,37 +2,16 @@ import requests
 import json
 
 api_key = 'd1158b740a06486ee301974b05f77baf'
-city = 'Boston'
+city = 'Chicago'
 country = 'us'
-
-def get_temp(arg):
-    for items in arg:
-        for key, value in items['main'].items():
-            if key=='temp':
-                print(value)
-                return(value)
-
-def get_pressure(arg):
-    for items in arg:
-        for key, value in items['main'].items():
-            if key=='pressure':
-                return(value)
-
-def get_temp_min(arg):
-    for items in arg:
-        for key, value in items['main'].items():
-            if key=='temp_min':
-                return(value)
 
 def current_weather():
     cw = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + api_key)
     data = cw.json()
     print("CITY: " + data["name"] + ", COUNTRY: " + data["sys"]["country"] + ", WEATHER: " + data["weather"][0]["description"] + ", MIN_TEMP: " + str(data["main"]["temp_min"]) + ", MAX_TEMP: " + str(data["main"]["temp_max"])) 
-    return(data)
-
-
-
+    
 current_weather()
+   
 
 ## {u'clouds': {u'all': 75}, u'name': u'Boston', u'visibility': 16093,
 ##  u'sys': {u'country': u'US', u'sunset': 1542144187, u'message': 0.0055, u'type': 1, u'id': 1273, u'sunrise': 1542108825},
@@ -41,9 +20,6 @@ current_weather()
 ##  u'main': {u'pressure': 1008, u'temp_min': 276.45, u'temp_max': 283.15, u'temp': 279.9, u'humidity': 76}, u'id': 4930956,
 ##  u'wind': {u'gust': 8.2, u'speed': 5.7, u'deg': 310},
 ##  u'cod': 200}
-
-## TODO: Sonu
-
 
 
 
