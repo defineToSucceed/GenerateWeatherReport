5a8d9f37b2063e17abfc35d183efa8ba8d6ffc6b
Ravi <rhonakere@salesforce.com>
2018-11-14 14:37:53 -0800

    capturing requited attributes

--- python/five_day_forecast.py
+++ python/five_day_forecast.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 api_key = 'd1158b740a06486ee301974b05f77baf'
-city = 'Boston'
+city = 'Chicago'
 country = 'us'
 
 def five_day_forecast():
     fd = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' + country + '&appid=' + api_key)
-    print(fd.json())
-    return(fd.json())
+    dict = fd.json()
+    select_data = dict['list']
 
+    for box in select_data:
+            print("CITY: " + dict["city"]["name"] + ", COUNTRY: " + dict["city"]["country"] + ", DATETIME: " + box['dt_txt'] + ", MIN_TEMP: " + str(box['main']['temp_min']) + ", MAX_TEMP: " + str(box['main']['temp_max']) + ", WEATHER: " + box['weather'][0]['description'])
+            
 five_day_forecast()
